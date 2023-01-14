225bf74c4f665db2de01559e5f170815e1b2d5e7
Walid MEDIA <informatique@esst-sup.com>
2023-01-14 10:47:26 +0100

    12 commit

--- membre/views.py
+++ membre/views.py
@@ -104,8 +104,8 @@ def checkout_session(request,plan_id):
 	    }],
 	    mode='payment',
 
-	    success_url='http://127.0.0.1:8000/pay_success?session_id={CHECKOUT_SESSION_ID}',
-	    cancel_url='http://127.0.0.1:8000/pay_cancel',
+	    success_url='https://web-production-f55f.up.railway.app/pay_success?session_id={CHECKOUT_SESSION_ID}',
+	    cancel_url='https://web-production-f55f.up.railway.app/pay_cancel',
 	    client_reference_id=plan_id
 	)
 	return redirect(session.url, code=303)
