bb57580ac62789e52e6dbb2d5940e3c2b746bce2
Walid MEDIA <informatique@esst-sup.com>
2023-02-06 15:30:54 +0100

    17 commit

--- ABLACKADABRA/settings.py
+++ ABLACKADABRA/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ABLACKADABRA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '0003',
@@ -93,15 +93,15 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5433',
     }
-}
+}"""
 
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 
 # Password validation
